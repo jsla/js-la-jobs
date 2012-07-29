add7f6ce3f46b4369a3c36eee8fae008e64503aa
David Guttman <david@davidguttman.com>
2012-07-29 15:44:08 -0700

    added browserid/persona client side code moved some stuff around

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,22 @@
-$(document).ready(function() {
-  $('body').timeago();
-})
+(function() {
+
+  var auth = function(token) {
+    var opts = {
+        url: '/login'
+      , success: function() {
+          console.log('success!!')
+        }
+      , data: {token:token}
+    }
+    $.ajax(opts)
+  }
+
+  $(document).ready(function() {
+    $('body').timeago()
+
+    $('.login').on('click', function() {
+      navigator.id.get(auth)
+    })
+  })
+
+}).call(this)
